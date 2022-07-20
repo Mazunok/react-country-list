795ccd7c50cc6692265953664f8b55a9d3a3d270
Mazunok <mazun.1999@icloud.com>
2022-07-20 19:43:45 +0300

    feat(country-list): add country list component

---
+++ src/componets/CountryList.tsx
@@ -0,0 +1,11 @@
+import { ICountry } from "../types";
+
+interface IProps {
+  countries: ICountry[];
+}
+
+export const CountryList = ({countries}: IProps) => {
+  return <ul>{countries.map(country =>{
+    return <li>{country.name}</li>
+  })}</ul>;
+};
